84aeddb4d690b793d17acb62a3f81881d59d9ee5
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2012-12-14 18:22:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openwwoof::Application.config.secret_token = '0dd5a336916acbfa3511b6520383991df406d3299188e948ae384c0fb174096849938e2206eba076dec9ec46b289e803dde0d3bbcba33ec9a780b5c258216511'
