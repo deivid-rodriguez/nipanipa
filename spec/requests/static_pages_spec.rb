718b26c8677084718d512bb8d0c171768ba0fe08
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-01-08 11:40:59 +0100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,13 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Openwwoof')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+    it "should not have a custom title page" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
