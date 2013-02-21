deivid-rodriguez/nipanipa

commit 0f01b8fbe395eab57c6888aebe6776d1ff13e40d

Add test coverage gem and implement some missing tests (donations and robots.txt)

app/models/user.rb
@@ -82,8 +82,8 @@ class User < ActiveRecord::Base
   end
 
   # Use a single partial path for all subclasses
-  def to_partial_path
-    "users/user"
-  end
+# def to_partial_path
+#   "users/user"
+# end
 
 end

.gitignore
@@ -19,3 +19,6 @@
 
 # Ignore application configuration
 /config/application.yml
+
+# SimpleCov results
+/coverage

.rspec
@@ -1,2 +1,2 @@
---color
+--colour
 --drb

Gemfile
@@ -39,9 +39,12 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
   gem 'capybara', '2.0.2'
+  gem 'capybara-webkit', '0.14.1'
+  gem 'database_cleaner', '0.9.1'
   gem 'launchy', '2.1.2'
   gem 'rb-inotify', '0.8.8'
   gem 'libnotify', '0.5.9'
   gem 'factory_girl_rails', '4.2.0'
   gem 'webmock', '1.9.0'
+  gem 'simplecov', '0.7.1', require: false
 end

app/controllers/donations_controller.rb
@@ -18,9 +18,9 @@ class DonationsController < ApplicationController
     response = Donation.send_pdt_post(params[:tx])
 
     if response.body.lines.first.chomp == 'SUCCESS'
-      flash.now[:notice] = t('donations.new.flash_notice')
+      flash.now[:notice] = t('donations.new.flash_success')
     else
-      flash.now[:alert] = t('donations.new.flash_alert')
+      flash.now[:alert] = t('donations.new.flash_error')
     end
     render 'static_pages/home'
   end

app/controllers/users_controller.rb
@@ -14,10 +14,7 @@ class UsersController < Devise::RegistrationsController
   # Override default devise update action to allow blank password (meaning no
   # change)
   def update
-    if params[:user][:password].blank?
-      params[:user].delete("password")
-      params[:user].delete("password_confirmation")
-    end
+    remove_blank_password_from_params
 
     @user = User.find(current_user.id)
     if @user.update_attributes(params[:user])
@@ -45,6 +42,13 @@ class UsersController < Devise::RegistrationsController
 
     def resource_class
       params[:type].present? ? params[:type].classify.constantize : super
+    end
+
+    def remove_blank_password_from_params
+      if params[:user][:password].blank?
+        params[:user].delete("password")
+        params[:user].delete("password_confirmation")
+      end
     end
 
 end

config/locales/en.yml
@@ -87,8 +87,8 @@ work?"
     signout: "Sign out"
   donations:
     new:
-      flash_notice: "Your donation was successful!"
-      flash_alert: "Sorry, there was some problem with your donation."
+      flash_success: "Your donation was successful!"
+      flash_error: "Sorry, there was some problem with your donation."
       title: "Contribute to NiPaNiPa"
       donate_title: "Donate"
       donate_desc: "NiPaNiPa needs your help to survive in an independent

config/locales/es.yml
@@ -89,8 +89,8 @@ buscando"
     signout: "Salir"
   donations:
     new:
-      flash_notice: "Tu donación fue exitosa!"
-      flash_alert: "Lo siento, hubo un problema con tu donación."
+      flash_sucess: "Tu donación fue exitosa!"
+      flash_error: "Lo siento, hubo un problema con tu donación."
       title: "Contribuye a NiPaNiPa"
       donate_title: "Dona!"
       donate_desc: "NiPaNiPa necesita de tu ayuda para seguir existiendo de

commit da266d98d81bbe34c3470c4171bc5e9a77bf7093

Fix bug introduced in commit 0f01b8f

app/models/user.rb
@@ -82,9 +82,9 @@ class User < ActiveRecord::Base
   end
 
   # Use a single partial path for all subclasses
-# def to_partial_path
-#   "users/user"
-# end
+  def to_partial_path
+    "users/user"
+  end
 
   def feedback_pairs
     result_list  = []