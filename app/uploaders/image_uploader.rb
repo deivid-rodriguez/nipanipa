c481fef4c0920096cbba982996469ef13d9b0b82
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-07-03 00:01:33 +0200

    Fix some styles and links

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -45,7 +45,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :small do
-    process resize_to_limit: [140, 140]
+    process resize_to_limit: [138, 138]
   end
 
   version :medium do
