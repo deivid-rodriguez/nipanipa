1598abc4d04a06cc26531a718f52de3c948db8ad
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-07-15 16:34:10 +0200

    Prettier message list

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,6 +40,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [40, 40]
   end
 
+  version :thumb_message do
+    process resize_to_fill: [69, 69]
+  end
+
   version :small_cropped do
     process resize_to_fill: [140, 140]
   end
