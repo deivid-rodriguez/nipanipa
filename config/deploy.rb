01bdd81d377e0a08b8a19a003bbde860cd25163d
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-07-18 00:31:04 +0200

    Make sure you push changes before deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,4 +22,15 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "ERROR: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+  before "deploy:cold", "deploy:check_revision"
 end
