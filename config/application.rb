c529ef366b35dc8c9b4b0e38ba0523629b68628c
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-08-05 12:36:31 +0200

    Remove figaro gem.
    It does very little job easily achieved manually.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,13 @@ if defined?(Bundler)
   Bundler.require(:default, :assets, Rails.env)
 end
 
+# Load per environment configuration
+config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+config.merge! config.fetch(Rails.env, {})
+config.each do |key, value|
+  ENV[key] = value.to_s unless value.kind_of? Hash
+end
+
 module Nipanipa
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
