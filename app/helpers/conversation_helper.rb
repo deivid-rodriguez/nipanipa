07ad87abafb19e7b052dab41a2a8929082b317ec
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-08-27 17:25:53 +0200

    Improve conversation format

--- app/helpers/conversation_helper.rb
+++ app/helpers/conversation_helper.rb
@@ -2,4 +2,8 @@ module ConversationHelper
   def other_user(conv)
     current_user == conv.from ? conv.to : conv.from
   end
+
+  def from_starter?(message)
+    message.from == message.conversation.from
+  end
 end
