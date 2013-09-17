a10dc76751072358acf02d5ed494e4ca5fb7eb80
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-09-17 14:53:39 +0200

    "I18n." not necessary here

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -7,7 +7,7 @@ describe UserMailer do
 
     it 'sends notification to recpient' do
       mail.subject.should eq(
-        I18n.t('user_mailer.message_reception.subject', from: conv.from.name))
+        t('user_mailer.message_reception.subject', from: conv.from.name))
       mail.to.should eq([conv.to.email])
       mail.from.should eq(['notifications@nipanipa.com'])
       mail.body.encoded.should match(conversation_url(conv))
