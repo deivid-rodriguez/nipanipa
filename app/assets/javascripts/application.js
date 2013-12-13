c9f789cc6fae718cade6e4409642f73640261ba9
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-12-13 11:45:20 +0100

    Wrap line to 80 chars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at
 // the bottom of the compiled file.
 //
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
+// Read https://github.com/sstephenson/sprockets#sprockets-directives for
+// details about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
