4c2d077fcfcd18389301831e726a26a6443e7d49
David Rodríguez <deivid.rodriguez@gmail.com>
2014-11-13 13:40:08 +0100

    Actually we don't want that in tests at all

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,9 +44,6 @@ Rails.application.configure do
   # Middleware
   config.middleware.use SpoofIp, ENV['FAKE_IP']
 
-  # Use Pry as the default console
-  config.console = Pry unless ENV['CI']
-
   # Your secret key is used for verifying the integrity of signed cookies. If
   # you change this key, all old signed cookies will become invalid!
   config.secret_key_base = 'x' * 30
