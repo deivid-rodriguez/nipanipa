7ed06f4d037cf800b0218cf9c4b2abebbc2905db
David Rodríguez <deivid.rodriguez@gmail.com>
2014-11-13 14:57:17 +0100

    Just drop that, not using Pry

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,9 +34,6 @@ Rails.application.configure do
   # Middleware
   config.middleware.use SpoofIp, ENV['IP']
 
-  # Use Pry as the default console
-  config.console = Pry
-
   # Your secret key is used for verifying the integrity of signed cookies. If
   # you change this key, all old signed cookies will become invalid!
   config.secret_key_base = 'x' * 30
