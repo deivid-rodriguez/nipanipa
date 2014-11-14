3025abb3bcf057115f9c04842c07b35e71cbdc11
David Rodríguez <deivid.rodriguez@gmail.com>
2014-11-14 14:41:34 +0100

    Initial user page in admin console

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,14 +1,22 @@
 ActiveAdmin.register User do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  permit_params :name, :email, :state, :region, :karma
+
+  index do
+    column :name
+    column :email
+    column :state
+    column :region
+    column :karma
+
+    actions
+  end
+
+  form do |f|
+    f.input :country
+    f.input :region
+
+    f.actions
+  end
+
+  filter :country
 end
