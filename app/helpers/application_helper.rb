9271d9529cf7cfa2e03517499d5977e07785e28b
David Rodríguez <deivid.rodriguez@gmail.com>
2014-11-17 19:31:07 +0100

    Standarize comment style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@
 # Miscelaneous helpers for rendering views
 #
 module ApplicationHelper
+  #
   # Returns the full title on a per-page basis
+  #
   def full_title(page_title)
     base_title = 'NiPaNiPa'
     if page_title.empty?
