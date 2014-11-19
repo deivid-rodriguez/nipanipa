b3db6956339a18662fd03e087c07abaf9a1bdfec
David Rodríguez <deivid.rodriguez@gmail.com>
2014-11-19 10:00:21 +0100

    Remove unused traits

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -33,26 +33,6 @@ FactoryGirl.define do
       availability []
     end
 
-    trait :with_work_types do
-      transient { count 1 }
-
-      after(:build) do |h, eval|
-        eval.count.times do
-          h.work_types << FactoryGirl.build(:work_type)
-        end
-      end
-
-      before(:create) { |h| h.work_types.each(&:save!) }
-    end
-
-    trait :with_language do
-      after(:build) do |v|
-        v.languages << FactoryGirl.build(:language)
-      end
-
-      before(:create) { |v| v.languages.each(&:save!) }
-    end
-
     factory :host, class: 'Host' do
       sequence(:name) { |n| "Host #{n}" }
       description 'We are a test host. We live in the countryside in the wild'
