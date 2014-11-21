0bc6727c98407511851a3590298db8a438e00ac1
David Rodríguez <deivid.rodriguez@gmail.com>
2014-11-21 18:10:46 +0100

    Fix continent names in admin console

--- app/models/continent.rb
+++ app/models/continent.rb
@@ -5,4 +5,8 @@ class Continent < ActiveRecord::Base
   validates :code, presence: true, uniqueness: true
 
   has_many :countries, dependent: :destroy
+
+  def name
+    I18n.t("continents.#{code.downcase}")
+  end
 end
