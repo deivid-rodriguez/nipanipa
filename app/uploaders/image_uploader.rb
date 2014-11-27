3f8602c5a1718dc1d1bdd86619f5f61f78ca2239
David Rodríguez <deivid.rodriguez@gmail.com>
2014-11-27 14:34:22 +0100

    Remove comments

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,13 +2,11 @@
 # Image Uploader required by CarrierWave
 #
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  # Internal image processor
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
