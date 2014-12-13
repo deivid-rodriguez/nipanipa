df44f0d4759baa0a3bb69532c760b9697001d8b5
David Rodríguez <deivid.rodriguez@gmail.com>
2014-12-13 18:52:21 +0100

    Better carrierwave errors in development

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,12 @@
-if Rails.env.test?
+unless Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :file
-    config.enable_processing = false
+    config.ignore_integrity_errors = false
+    config.ignore_processing_errors = false
+    config.ignore_download_errors = false
+
+    if Rails.env.test?
+      config.storage = :file
+      config.enable_processing = false
+    end
   end
 end
