5f3a52c4ef8ef784bc7d43cb5ef0d2e93d0d6e5f
David Rodríguez <deivid.rodriguez@gmail.com>
2015-07-11 16:10:02 -0300

    Validate languages

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,6 +2,8 @@
 # A language to communicate with people
 #
 class Language < ActiveRecord::Base
+  validates :code, presence: true, uniqueness: true
+
   has_many :language_skills
   has_many :users, through: :language_skills
 end
