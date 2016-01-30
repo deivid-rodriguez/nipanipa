59ffbb44464230ff48a1d8a76bce76b3d22c5e43
David Rodríguez <deivid.rodriguez@gmail.com>
2016-01-30 17:34:06 +0100

    Typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  config.logger = SyslogLogger.new
+  config.logger = Syslog::Logger.new
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
