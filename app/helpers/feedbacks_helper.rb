d8d673443a685edae896b1a50428326e40057f28
David Rodríguez <deivid.rodriguez@gmail.com>
2016-02-29 13:29:01 +0100

    Style. Whitespace

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -17,11 +17,13 @@ module FeedbacksHelper
     received_count = feedbacks.count { |f| f[0] }
     sent_count = feedbacks.count { |f| f[1] }
     count_str = t("feedbacks.feedbacks.title")
+
     if received_count != 0 || sent_count != 0
       received = t("feedbacks.received", count: received_count)
       sent = t("feedbacks.sent", count: sent_count)
       count_str += format(" (%s, %s)", received, sent)
     end
+
     count_str
   end
 end
