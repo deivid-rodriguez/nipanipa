9b43da96414c24a4eda7c304bf3389394597cf05
David Rodríguez <deivid.rodriguez@gmail.com>
2016-02-29 13:28:30 +0100

    Style. Add parenthesis to `I18n.t` call

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -16,7 +16,7 @@ module FeedbacksHelper
   def feedback_count(feedbacks)
     received_count = feedbacks.count { |f| f[0] }
     sent_count = feedbacks.count { |f| f[1] }
-    count_str = t "feedbacks.feedbacks.title"
+    count_str = t("feedbacks.feedbacks.title")
     if received_count != 0 || sent_count != 0
       received = t("feedbacks.received", count: received_count)
       sent = t("feedbacks.sent", count: sent_count)
