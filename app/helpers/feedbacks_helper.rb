0a52a9a51c70e2b26d9a1e6418a29f66337d3f2a
David Rodríguez <deivid.rodriguez@gmail.com>
2016-02-29 13:26:09 +0100

    Move feedback helper to FeedbacksHelper

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -12,4 +12,16 @@ module FeedbacksHelper
 
     edit_user_feedback_path(to, feedback)
   end
+
+  def feedback_count(feedbacks)
+    received_count = feedbacks.count { |f| f[0] }
+    sent_count = feedbacks.count { |f| f[1] }
+    count_str = (t "feedbacks.feedbacks.title").to_s
+    if received_count != 0 || sent_count != 0
+      received = t("feedbacks.received", count: received_count)
+      sent = t("feedbacks.sent", count: sent_count)
+      count_str += format(" (%s, %s)", received, sent)
+    end
+    count_str
+  end
 end
