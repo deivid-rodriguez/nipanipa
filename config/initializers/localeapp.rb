47c9b725b184aebd491e1ef03a886b554f6b730a
David Rodríguez <deivid.rodriguez@gmail.com>
2016-03-08 10:33:39 +0100

    Tweak locale app configuration

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -3,5 +3,19 @@
 require "localeapp/rails"
 
 Localeapp.configure do |config|
+  #
+  # Set up API keys
+  #
   config.api_key = "JTt7kLxvGkfMcE88EL9QkdN2iVxFq4neTOhJLQejJ75VWPcuIb"
+
+  #
+  # Ignore simple_form & activerecord keys
+  #
+  config.blacklisted_keys_pattern = /simple_form|activerecord/
+
+  #
+  # Don't autohandle anything
+  #
+  config.sending_environments = []
+  config.polling_environments = []
 end
