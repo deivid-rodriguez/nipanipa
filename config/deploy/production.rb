00fc1511a07a556b8473975a90ef56c29b317da4
David Rodríguez <deivid.rodriguez@gmail.com>
2016-03-25 09:52:46 +0100

    Cleanup capistrano's config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,24 +18,12 @@ set :stage, :production
 # a hash can be used to set extended properties on the server.
 server "nipanipa.com", user: "deployer", roles: %w(web app db), primary: true
 
-# You can set custom ssh options.
-# It's possible to pass any option but you need to keep in mind
-# that net/ssh understand limited list of options.
-# You can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# Set it globally
+#
+# Forward ssh agent globally for all servers
+#
 set :ssh_options, forward_agent: true
 
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
-
+#
+# Set default Rails environment
+#
 fetch(:default_env)[:rails_env] = :production
