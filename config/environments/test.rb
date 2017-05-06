248651871962d27b1e64ef25e74111330b2772df
David Rodríguez <deivid.rodriguez@riseup.net>
2017-05-06 18:33:58 -0300

    Move all actionmailer test config to single place

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,12 +30,10 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-  config.action_mailer.perform_caching = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
+  # ActionMailer config
   config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
