aa4c8b860386b2b2728752973536494fe8a0b1fc
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-16 01:18:09 +0200

    Use default AA js file

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -166,7 +166,7 @@ ActiveAdmin.setup do |config|
   #   config.register_stylesheet 'my_print_stylesheet.css', :media => :print
   #
   # To load a javascript file:
-  config.register_javascript "active_admin.js"
+  # config.register_javascript "my_javascript.js"
 
   # == CSV options
   #
